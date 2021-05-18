da698609b5c0d57b2956e835cfcae14ea9f7c975
JuliooCesarMDM <juliomatias123@outlook.com>
2021-05-18 16:41:51 -0300

    talvez um bom progresso

--- BARGenerator.py
+++ BARGenerator.py
@@ -1,41 +1,92 @@
 import datetime
 import pathlib
+from tabulate import tabulate
+import enum
+
+
+class SizeOf(enum.Enum):
+    ADDR_8_BITS = 1
+    ADDR_16_BITS = 2
+    ADDR_32_BITS = 4
+    ADDR_FLOAT = 4
+
+
+Table = [['Name', 'Size'],
+         ['KP_ACC_AHRS_ADDR', SizeOf.ADDR_8_BITS.value],
+         ['KI_ACC_AHRS_ADDR', SizeOf.ADDR_8_BITS.value],
+         ['KP_MAG_AHRS_ADDR', SizeOf.ADDR_8_BITS.value],
+         ['KI_MAG_AHRS_ADDR', SizeOf.ADDR_8_BITS.value]]
 
 
 def format_entry(x): return '%d' % round(x)
 
 
-def Generate_Defines(f, DefineName, ADDROffSet, ADDRMax):
+def Generate_Defines(Function, DefineName, ADDROffSet, ADDRMax):
 
-    for i in range(ADDRMax):
-        f.write('#define %s ' % DefineName)
-        f.write(format_entry(ADDROffSet))
-        if i != ADDRMax - 1:
-            f.write("\n")
+    for ADDRCount in range(ADDRMax):
+        Function.write('#define %s ' % DefineName)
+        Function.write(format_entry(ADDROffSet))
+        Function.write("\n")
 
 
-def Generate_Code(f, Date):
-    #GERA O TOPO DA EXTENSÃO
-    f.write('#pragma once\n\n')
-    f.write('//BASE ADDRESS REGISTER\n\n')
-    f.write(
-        '//ESSE ARQUIVO FOI GERADO AUTOMATICAMENTE - POR FAVOR EVITE DE EDITAR O MESMO MANUALMENTE!\n\n')
-    f.write('//ATUALIZADO EM %s\n\n' % Date)
+def Generate_Defines2(Function, DefineName, ADDROffSet):
+
+    Function.write('#define %s ' % DefineName)
+    Function.write(format_entry(ADDROffSet))
+    Function.write("\n")
+
+
+def Generate_Code(Function, Date):
+    # GERA O TOPO DA EXTENSÃO
+    Function.write("\
+/* \n\
+   Este arquivo faz parte da JCFLIGHT.\
+   \n\
+   JCFLIGHT é um software livre: você pode redistribuí-lo e/ou modificar \n\
+   sob os termos da GNU General Public License conforme publicada por \n\
+   a Free Software Foundation, seja a versão 3 da Licença, ou \n\
+   (à sua escolha) qualquer versão posterior. \n\
+   \n\
+   JCFLIGHT é distribuído na esperança de ser útil, \n\
+   mas SEM QUALQUER GARANTIA; sem mesmo a garantia implícita de \n\
+   COMERCIALIZAÇÃO ou ADEQUAÇÃO A UM DETERMINADO FIM. Veja o \n\
+   GNU General Public License para mais detalhes. \n\
+   \n\
+   Você deve ter recebido uma cópia da Licença Pública Geral GNU \n\
+   junto com a JCFLIGHT. Caso contrário, consulte <http://www.gnu.org/licenses/>. \n\
+*/\
+\n\n")
+
+    Function.write('#ifndef BAR_H_\n#define BAR_H_\n\n')
+
+    Function.write('//BASE ADDRESS REGISTER\n\n')
+    Function.write(
+        '//ESSE ARQUIVO FOI GERADO AUTOMATICAMENTE - POR FAVOR,NUNCA O EDITE MANUALMENTE!\n\n')
+    Function.write('//ATUALIZADO EM %s\n\n' % Date)
 
     # PRIMEIRO UPLOAD ADDR
-    f.write('//ADDR PARA VERIFICAR O PRIMEIRO UPLOAD DO FIRMWARE\n')
-    f.write('#define FIRMWARE_FIRST_USAGE_ADDR 1500\n\n')
-    
-    #APENAS TESTE POR ENQUANTO - SERÁ NECESSARIO CRIAR UMA TABELA CONTENDO O NOME DO PARAMETRO E TAMANHO QUE ELE OCUPA (OffSet)
-    Generate_Defines(f, 'KP_ACC_AHRS_ADDR', 1, 1000)
+    Function.write('//ADDR PARA VERIFICAR O PRIMEIRO UPLOAD DO FIRMWARE\n')
+    Function.write('#define FIRMWARE_FIRST_USAGE_ADDR 1500\n\n')
+
+    # APENAS DE TESTE POR ENQUANTO - SERÁ NECESSARIO CRIAR UMA TABELA CONTENDO O NOME DO PARAMETRO E O TAMANHO QUE ELE OCUPA PARA O OFFSET
+    #Generate_Defines(Function, 'KP_ACC_AHRS_ADDR', 1, 1000)
+
+    #Generate_Defines(Function, Table, 1, 1)
+
+    for TableSizeCount in range(len(Table) - 1):
+        Generate_Defines2(Function, Table[TableSizeCount + 1][0], TableSizeCount)
+
+    Function.write('\n\n#endif\n')
+
+    print(tabulate(Table, headers='firstrow', tablefmt='fancy_grid'))
 
 
 if __name__ == '__main__':
 
-    output = pathlib.PurePath(__file__).parent / \
+    Output = pathlib.PurePath(__file__).parent / \
         'src' / 'BAR' / 'BARGEN.h'
 
-    date = datetime.datetime.now()
+    Date = datetime.datetime.now()
 
-    with open(output, 'w') as f:
-        Generate_Code(f, date)
+    with open(Output, 'w') as Function:
+        Generate_Code(Function, Date)
